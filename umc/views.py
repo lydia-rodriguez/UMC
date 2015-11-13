6cf1c4b78cf8a90801994ce79e9e4ee687303228
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2015-11-13 11:59:25 -0600

    Changed the HTML

--- umc/views.py
+++ umc/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import User_Request
 
 # Create your views here.
+def user_request_list(request):
+	user_requests = User_Request.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+	return render(request, 'umc/user_request_list.html', {})
+
