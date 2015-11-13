8abddf1282ab3b9ef4165bede8e1316146318e28
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2015-11-13 12:27:25 -0600

    Modified templates to display user_requests from DB.

--- umc/views.py
+++ umc/views.py
@@ -5,5 +5,6 @@ from .models import User_Request
 # Create your views here.
 def user_request_list(request):
 	user_requests = User_Request.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
-	return render(request, 'umc/user_request_list.html', {})
+	print(user_requests)
+	return render(request, 'umc/user_request_list.html', {'user_requests':user_requests})
 
