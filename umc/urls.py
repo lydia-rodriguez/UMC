d0513081276d9cd318a31aa7552b6a8484b46955
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2015-11-13 17:22:09 -0600

    Updated form, user_request_new view, updated header

--- umc/urls.py
+++ umc/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	url(r'^$', views.user_request_list, name='user_request_list'),
 	url(r'^user_request/(?P<pk>[0-9]+)/$', views.user_request_detail, name='user_request_detail'),
 	url(r'^user_request/new/$', views.user_request_new, name='user_request_new'),
+	url(r'^user_request/(?P<pk>[0-9]+)/edit/$', views.user_request_edit, name='user_request_edit'),
 ]
--- umc/views.py
+++ umc/views.py
@@ -17,7 +17,6 @@ def user_request_new(request):
 	if request.method == "POST":
 		form = User_Request_Form(request.POST)
 		if form.is_valid():
-			print(form.fields)
 			user_request = form.save(commit=False)
 			user_request.requestor_emp_id = request.POST['requestor_emp_id']
 			user_request.user_emp_id = request.POST['user_emp_id']
@@ -25,6 +24,20 @@ def user_request_new(request):
 			user_request.save()
 			return redirect('umc.views.user_request_detail', pk=user_request.id)
 	else:
-		print("Hello World")
 		form = User_Request_Form()
+	return render(request, 'umc/user_request_new.html', {'form': form})
+	
+def user_request_edit(request, pk):
+	user_request = get_object_or_404(User_Request, pk=pk)
+	if request.method == "POST":
+		form = User_Request_Form(request.POST, instance=user_request)
+		if form.is_valid():
+			user_request = form.save(commit=False)
+			user_request.requestor_emp_id = request.POST['requestor_emp_id']
+			user_request.user_emp_id = request.POST['user_emp_id']
+			user_request.client_name = request.POST['client_name']
+			user_request.save()
+			return redirect('umc.views.user_request_detail', pk=user_request.id)
+	else:
+		form = User_Request_Form(instance=user_request)
 	return render(request, 'umc/user_request_edit.html', {'form': form})
