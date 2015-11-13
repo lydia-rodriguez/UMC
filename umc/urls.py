039c068b409da2b00ca3491130f907fb2a72c28f
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2015-11-13 17:25:19 -0600

    Changed landing page, urls

--- umc/urls.py
+++ umc/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.user_request_list, name='user_request_list'),
+#	url(r'^$', views.user_request_list, name='user_request_list'),
+	url(r'^$', views.user_request_new, name='user_request_new'),
 	url(r'^user_request/(?P<pk>[0-9]+)/$', views.user_request_detail, name='user_request_detail'),
 	url(r'^user_request/new/$', views.user_request_new, name='user_request_new'),
 	url(r'^user_request/(?P<pk>[0-9]+)/edit/$', views.user_request_edit, name='user_request_edit'),
+	url(r'^user_request/list/$', views.user_request_list, name='user_request_list'),
 ]
